da47e7f96e61e5234cc50e3e897048177565a35b
Dmitry Zaitsev <d.zaitsev@corp.mail.ru>
2020-10-22 21:48:08 +0300

    lesson6

---
+++ lesson6/main.js
@@ -0,0 +1,41 @@
+// class Computer {
+//   constructor (name) {
+//     this.name = name
+//   }
+
+//   printName () {
+//     console.log(this)
+//     console.log(this.name)
+//   }
+// }
+
+// class Human {
+//   constructor (name) {
+//     this.name = name
+//   }
+
+//   printName () {
+//     console.log(this.name)
+//   }
+// }
+
+// const MyComputer = new Computer('Asus');
+// const MrHuman = new Human('Alex');
+
+// // MyComputer.printName()
+// // MrHuman.printName()
+
+
+
+// const obj = {
+//   0: { name: 'Alex' },
+//   1: { name: 'Kate' },
+//   2: { name: 'Barny' },
+//   length: 3,
+// }
+
+// Array.prototype.forEach.call(obj, o => {
+//   console.log(o)
+// })
+
+
